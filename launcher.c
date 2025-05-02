ab57703f22d889ad43b1f1a62c9f23d21f5f6b01
N-MTN <nicolas.matringhen@gmail.com>
2025-05-02 10:49:03 +0200

    For 27/04/2025 Check (bmp 24)

---
+++ launcher.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "launcher_func.h"
+
+int main(){
+    val = load_menu();
+
+    return 0;
+}
---
+++ launcher_func.c
@@ -0,0 +1,7 @@
+#include "launcher_func.h"
+int load_menu(){
+  int val = 0;
+
+
+  return val;
+}
