e2f2f26540f322dcea116fd59cefd3634d5de252
N-MTN <nicolas.matringhen@gmail.com>
2025-05-02 10:54:33 +0200

    For 27/04/2025 Check (bmp 24)

--- launcher_func.c
+++ launcher_func.c
@@ -1,7 +1,15 @@
 #include "launcher_func.h"
+
 int load_menu(){
   int val = 0;
+  printf("Welcome to our menu :\n");
+  printf("  1. Open an Image\n");
+  printf("  2. Save an Image\n");
+  printf("  3. Apply a filter\n");
+  printf("  4. Display image information\n");
+  printf("  5. Quit\n");
 
-
+  scanf("%d",&val);
+  printf(">>> Your choice : %d \n,val");
   return val;
 }
